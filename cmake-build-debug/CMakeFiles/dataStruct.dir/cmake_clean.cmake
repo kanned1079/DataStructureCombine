cc673138c785fbfbe28dc0d09a76d31ffd3f4347
kanned1079 <kanned1079@gmail.com>
2023-11-19 19:27:27 +0800

    Finish BiTree operation.

---
+++ cmake-build-debug/CMakeFiles/dataStruct.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dataStruct.dir/main.c.o"
+  "CMakeFiles/dataStruct.dir/main.c.o.d"
+  "dataStruct"
+  "dataStruct.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dataStruct.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
